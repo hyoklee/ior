66d9bab6cbccaa8dc9a56c2e545d57cc0ab5cd32
github-action-benchmark <github@users.noreply.github.com>
2025-09-22 14:08:01 +0000

    add IOR HDF5 Serial Performance (customBiggerIsBetter) benchmark result for 9529b6c6490b109743882f9789ca27076ca352be

---
+++ dev/bench-serial/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1758550081982,
+  "repoUrl": "https://github.com/hyoklee/ior",
+  "entries": {
+    "IOR HDF5 Serial Performance": [
+      {
+        "commit": {
+          "author": {
+            "email": "hyoklee@hdfgroup.org",
+            "name": "H. Joe Lee",
+            "username": "hyoklee"
+          },
+          "committer": {
+            "email": "hyoklee@hdfgroup.org",
+            "name": "H. Joe Lee",
+            "username": "hyoklee"
+          },
+          "distinct": true,
+          "id": "9529b6c6490b109743882f9789ca27076ca352be",
+          "message": "ci: boostrap",
+          "timestamp": "2025-09-22T09:03:12-05:00",
+          "tree_id": "7c267bf6de1b03eefec50d11d1a93067ccc05c18",
+          "url": "https://github.com/hyoklee/ior/commit/9529b6c6490b109743882f9789ca27076ca352be"
+        },
+        "date": 1758550081172,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "No Results Found",
+            "value": 0,
+            "unit": "N/A"
+          }
+        ]
+      }
+    ]
+  }
+}
