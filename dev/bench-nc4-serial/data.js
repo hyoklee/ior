45f738f04d79f01f3502cebb9f34bccadf5912b0
github-action-benchmark <github@users.noreply.github.com>
2025-10-26 22:58:44 +0000

    add IOR NetCDF-4 Serial Performance (customBiggerIsBetter) benchmark result for 1e7b41052edb1c36971c87d617e6353ecd07c109

---
+++ dev/bench-nc4-serial/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1761519524006,
+  "repoUrl": "https://github.com/hyoklee/ior",
+  "entries": {
+    "IOR NetCDF-4 Serial Performance": [
+      {
+        "commit": {
+          "author": {
+            "email": "hyoklee@hdfgroup.org",
+            "name": "H. Joe Lee",
+            "username": "hyoklee"
+          },
+          "committer": {
+            "email": "hyoklee@hdfgroup.org",
+            "name": "H. Joe Lee",
+            "username": "hyoklee"
+          },
+          "distinct": true,
+          "id": "1e7b41052edb1c36971c87d617e6353ecd07c109",
+          "message": "ci: use my repo",
+          "timestamp": "2025-10-26T15:39:00-05:00",
+          "tree_id": "e26163265d5ef1b7e3eab6311bb07a49f70aa62d",
+          "url": "https://github.com/hyoklee/ior/commit/1e7b41052edb1c36971c87d617e6353ecd07c109"
+        },
+        "date": 1761519522666,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Test 4: HDF5 Backend Test (for comparison) - Write Throughput",
+            "value": 871.96,
+            "unit": "MiB/sec"
+          },
+          {
+            "name": "Test 4: HDF5 Backend Test (for comparison) - Read Throughput",
+            "value": 7223.59,
+            "unit": "MiB/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
