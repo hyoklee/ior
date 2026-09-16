231d337e7d4f68c86e507c6f4610323c05478a3a
github-action-benchmark <github@users.noreply.github.com>
2026-09-16 10:25:03 +0000

    add IOR HDF5 Parallel Performance (customBiggerIsBetter) benchmark result for 2d9f6d6d7e34198548150000a4523de5ae09181c

--- dev/bench-parallel/data.js
+++ dev/bench-parallel/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789296298912,
+  "lastUpdate": 1789554302990,
   "repoUrl": "https://github.com/hyoklee/ior",
   "entries": {
     "IOR HDF5 Parallel Performance": [
@@ -10545,6 +10545,68 @@ window.BENCHMARK_DATA = {
           {
             "name": "Test 4: Larger Collective Test - Read Throughput",
             "value": 12296.41,
+            "unit": "MiB/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "H. Joe Lee",
+            "username": "hyoklee",
+            "email": "hyoklee@hdfgroup.org"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "2d9f6d6d7e34198548150000a4523de5ae09181c",
+          "message": "Merge branch 'hpc:main' into main",
+          "timestamp": "2026-08-03T21:38:39Z",
+          "url": "https://github.com/hyoklee/ior/commit/2d9f6d6d7e34198548150000a4523de5ae09181c"
+        },
+        "date": 1789554301644,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Test 1: Small File Test (1MB per process, 2 processes) - Write Throughput",
+            "value": 262.29,
+            "unit": "MiB/sec"
+          },
+          {
+            "name": "Test 1: Small File Test (1MB per process, 2 processes) - Read Throughput",
+            "value": 1926.64,
+            "unit": "MiB/sec"
+          },
+          {
+            "name": "Test 2: Medium File Test (10MB per process, 2 processes) - Write Throughput",
+            "value": 1961.56,
+            "unit": "MiB/sec"
+          },
+          {
+            "name": "Test 2: Medium File Test (10MB per process, 2 processes) - Read Throughput",
+            "value": 11408.42,
+            "unit": "MiB/sec"
+          },
+          {
+            "name": "Test 3: Collective I/O Test - Write Throughput",
+            "value": 1098.19,
+            "unit": "MiB/sec"
+          },
+          {
+            "name": "Test 3: Collective I/O Test - Read Throughput",
+            "value": 7385.64,
+            "unit": "MiB/sec"
+          },
+          {
+            "name": "Test 4: Larger Collective Test - Write Throughput",
+            "value": 1608.49,
+            "unit": "MiB/sec"
+          },
+          {
+            "name": "Test 4: Larger Collective Test - Read Throughput",
+            "value": 13522.38,
             "unit": "MiB/sec"
           }
         ]
